8542e049506ab6940782d3d98a2d6822ace9c9ad
Harjot <hsingh172000@gmail.com>
2020-06-17 22:29:53 -0700

    Added checking win condition,  Board function, and update anything that would have broke because of the new Board function

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
-var canvas = document.getElementById('canvas')
+let canvas = document.getElementById('canvas')
 /** @type {CanvasRenderingContext2D} */
-var ctx = canvas.getContext('2d')
+let ctx = canvas.getContext('2d')
 
 canvas.width = window.screen.width
 canvas.height = window.screen.height
 
-var circles = new Array(6)
-// Create circles for board
-for (var i = 1; i <= 6; i++) {
-    var row = new Array()
-    for (var j = 1; j <= 7; j++) {
-        row.push(new Circle(i, j))
-    }
-    circles[i-1] = row
-}
-console.log(circles)
+let turn = 0
+let board = new Board()
+board.init_board()
 
-setInterval(loop, 1)
+setInterval(loop, 100)
 
 function Circle(i, j) {
     this.col = j-1
@@ -26,7 +19,6 @@ function Circle(i, j) {
     this.start_angle = 0;
     this.end_angle = 2 * Math.PI
     this.color = "#FFFFFF"
-    console.log(this.col)
 
     this.draw = function() {
         ctx.beginPath()
@@ -39,41 +31,137 @@ function Circle(i, j) {
     }
 
     this.check_hit = function(x, y) {
-        var dist = Math.sqrt((this.x-x) ** 2 + (this.y-y) ** 2)
-        if (dist < this.r) {
-            return true
-        }
-        return false
+        let dist = Math.sqrt((this.x-x) ** 2 + (this.y-y) ** 2)
+        if (dist < this.r) return true
+        else return false
     }
 }
 
-function update_board(col) {
-    for (var i = 0; i < circles.length; i++) {
-        if (circles[i][col].color == "#5DEED6") break
+function Board() {
+    this.m = 6
+    this.n = 7  
+    this.player_turn = 0
+    this.board = []
+    this.last_row = -1
+    this.last_col = -1
+
+    this.init_board = function() {
+        this.board = []
+        this.player_turn = 0
+        for (let i = 1; i <= 6; i++) {
+            let row = []
+            for (let j = 1; j <= 7; j++) {
+                row.push(new Circle(i, j))
+            }
+            this.board.push(row)
+        }
+        console.log(this.board)
+        console.log("Board initialized!")
     }
 
-    if (i != 0) circles[i-1][col].color = "#5DEED6"
+    this.draw = function() {
+        this.board.forEach((row) => {
+            row.forEach((circle) => {
+                circle.draw()
+            })
+        })
+    }
+
+    this.update_board = function(col) {
+        for (var i = 0; i < this.board.length; i++) {
+            if (this.board[i][col].color != "#FFFFFF") break
+        }
+
+        if (i != 0) this.board[i-1][col].color = this.player_turn == 0 ? "#5DEED6" : "#FFC300"
+        this.last_row = i-1
+        this.last_col = col
+    }
+
+    this.place = function(x, y) {
+        this.board.forEach((row) => {
+            row.forEach((circle) => {
+                let hit = circle.check_hit(x, y)
+                if (hit) { 
+                    this.update_board(circle.col)
+                    this.player_turn ^= 1
+                }
+            })
+        })
+    } 
+
+    this.reset_board = function() {
+        this.init_board()
+    }
+    
+
+    this.check_win = function() {
+        if (this.last_row == -1 || this.last_col == -1) return false
+    
+        let prev_player = (this.player_turn ^ 1) == 0 ? "#5DEED6" : "#FFC300"
+
+        // Horizontal win
+        let streak = 0
+        for (let col = 0; col < this.n; col++) {
+            if (streak == 4) return true
+            if (this.board[this.last_row][col].color == prev_player) streak++
+            else streak = 0
+        }
+    
+        if (streak == 4) return true
+        
+        streak = 0
+        // Vertical win
+        for (let row = this.m-1; row >= 0; row--) {
+            if (streak == 4) return true
+            if (this.board[row][this.last_col].color == prev_player) streak++
+            else streak = 0
+        }
+
+        if (streak == 4) return true
+
+        // Left diagonal win
+        streak = 0
+        let row = this.last_row
+        let col = this.last_col
+        while (row < this.m && col >= 0) {
+            if (streak == 4) return true
+            if (this.board[row][col].color == prev_player) streak++
+            else streak = 0
+            row++
+            col--
+        }
+
+        if (streak == 4) return true
+        // Right diagonal win
+        streak = 0
+        row = this.last_row
+        col = this.last_col
+        while (row < this.m && col < this.n) {
+            console.log(streak, row, col, this.board[row][this.last_col].color, prev_player)
+            if (streak == 4) return true
+            if (this.board[row][col].color == prev_player) streak++
+            else streak = 0
+            row++
+            col++
+        }
+    }
 }
 
 function loop() {
     // Draw the circles to the screen
-    circles.forEach((row) => {
-        row.forEach((circle) => {
-            circle.draw()
-        })
-    })
+    board.draw()
+    let win = board.check_win()
 }
 
 // event handler on canvas to check for clicks on page
 // specifically the circles
 canvas.addEventListener('click', (e) => {
-    circles.forEach((row) => {
-        row.forEach((circle) => {
-           var hit =  circle.check_hit(e.clientX, e.clientY)
-           if (hit) {
-               console.log(circle.col)
-               update_board(circle.col)
-           }
-        })
-    })
+    board.place(e.clientX, e.clientY)
+})
+
+canvas.addEventListener('keydown', (e) => {
+    if (e.keyCode == 82) {
+        board.reset_board()
+        console.log("Reset!")
+    }
 })
